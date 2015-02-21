abc8173670960db9e98c7ff59e1378af4b6c295d
rentze <r.tzeferakos@gmail.com>
2015-02-21 18:18:26 +0100

    best and hospitalrank
    best.R
    hospitalRank.R

--- Week 4/rankhospital.R
+++ Week 4/rankhospital.R
@@ -1,22 +1,4 @@
-## Write a function called best that take two arguments: the 2-character abbreviated name of a state and an
-## outcome name. The function reads the outcome-of-care-measures.csv file and returns a character vector
-## with the name of the hospital that has the best (i.e. lowest) 30-day mortality for the specified outcome
-## in that state. The hospital name is the name provided in the Hospital.Name variable. The outcomes can
-## be one of “heart attack”, “heart failure”, or “pneumonia”. Hospitals that do not have data on a particular
-## outcome should be excluded from the set of hospitals when deciding the rankings.
-
-
-## Handling ties. If there is a tie for the best hospital for a given outcome, then the hospital names should
-## be sorted in alphabetical order and the first hospital in that set should be chosen (i.e. if hospitals “b”, “c”,
-## and “f” are tied for best, then hospital “b” should be returned).
-
-
-
-
-best <- function(state, outcome) {
-  
- 
-  
+rankhospital <- function(state,outcome,num ="best"){
   ## Read outcome data
   ## need to read columns related to 30-day mortality for the specified outcome (“heart attack”, “heart failure”, or “pneumonia”)
   
@@ -26,13 +8,13 @@ best <- function(state, outcome) {
   ## [11] "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"  
   ## [17] "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
   ## [23] "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"   
-
+  
   outcomeVector <- c('heart attack'=11,'heart failure'=17,'pneumonia'=23)
   outcome <- outcomeVector[outcome]
   
   ## read data
   data <-  read.csv("outcome-of-care-measures.csv", colClasses = "character")
- 
+  
   
   ## Check that state is valid
   ## a NA in value causes that level to be removed from the levels and the elements formerly with that level to be replaced by NA.
@@ -47,16 +29,37 @@ best <- function(state, outcome) {
   {
     stop("invalid outcome")
   }
-
+  
+  ## Check whether num is a number or a valid string
+  
+  if (class(num) == "character"){
+    if (!(num=="best" || num=="worst"))
+      stop("invalid num value")
+  }
   
   ## Return hospital name in that state with lowest 30-day death
-
-  ## retrieve data for the input state
-
+  
+  
+  ## transform data for the input outcome
   data[,outcome] <- as.numeric(as.character(data[,outcome]))
   data<-na.omit(data)
-
-  filteredData <- subset(data, State == state)
-  filteredData <-filteredData[order(filteredData[,outcome],na.last=TRUE),"Hospital.Name"]
-  filteredData[1]
+  ##write.table(data,file='result.txt',sep='\t')
+  
+  ## retrieve data for the input state
+  filteredPerState <- subset(data, State == state)
+  
+  ##write.table(filteredPerState,file='result2.txt',sep='\t')
+  
+  filteredPerState <- filteredPerState[order(filteredPerState[,outcome],filteredPerState[,'Hospital.Name'],na.last=TRUE),'Hospital.Name']
+  ##write.table(na.omit(filteredPerState),file='result3.txt',sep='\t')
+  
+  if (num == 'best')
+    return(filteredPerState[1])
+  else if (num == 'worst')
+    return(filteredPerState[length(filteredPerState)])
+  else if (length(filteredPerState) < num)
+    return(NA)
+  else
+    return (filteredPerState[num])
+  
 }
